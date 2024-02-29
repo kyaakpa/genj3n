3a3d98c31dce445ce5e0488adb0b57cd7a4b4640
kenma-zks <pallawmgr20@gmail.com>
2024-02-29 20:45:57 +0545

    added navbar animations

--- app/page.js
+++ app/page.js
@@ -1,5 +1,5 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <div className="w-full h-screen bg-blue-300">Home</div>;
+  return <div className="w-full h-screen bg-[#F2F3F3] pt-10">Home</div>;
 }
