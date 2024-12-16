b73cae3f10d8e86c5076ca2a6cc3fca0bc133c07
Pallaw Magar <pallawmgr20@gmail.com>
2024-12-16 10:32:41 -0500

    remove spinner and place loading in center

--- app/payment-success/page.js
+++ app/payment-success/page.js
@@ -34,7 +34,7 @@ const SuccessContent = ({ router }) => (
 // Loading component
 const LoadingSpinner = () => (
   <div className="min-h-screen flex items-center justify-center">
-    <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-gray-900"></div>
+    <span className="text-gray-500">Loading...</span>
   </div>
 );
 
--- app/shop/page.jsx
+++ app/shop/page.jsx
@@ -104,7 +104,8 @@ const Page = () => {
 
         {isLoading ? (
           <div className="flex justify-center items-center h-48 md:h-64">
-            <div className="animate-spin rounded-full h-8 w-8 md:h-12 md:w-12 border-t-2 border-b-2 border-gray-900"></div>
+            {/* <div className="animate-spin rounded-full h-8 w-8 md:h-12 md:w-12 border-t-2 border-b-2 border-gray-900"></div> */}
+            <span className="text-gray-500">Loading...</span>
           </div>
         ) : paintings.length === 0 ? (
           <div className="flex justify-center items-center h-48 md:h-64">
